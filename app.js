4a770156fe8f079154bf0e2132a8be19ea438595
Andre Da Silva Carreira <andreecarreira@gmail.com>
2024-10-11 13:04:47 +0100

    Add arrow key scrolling

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const backgroundImage = document.getElementById("backgroundImage");
 const thumbnailGroup = document.getElementById("thumbnailGroup");
-let foucusIndex = 0;
 
 const images = [
   {
@@ -32,11 +31,14 @@ const images = [
 
 for (let index = 0; index < images.length; index++) {
   const img = document.createElement("img");
+  let targetIndex = 0;
 
   img.src = images[index].src;
   img.alt = images[index].alt;
   img.title = images[index].title;
   img.tabIndex = "1";
+  img.focusIndex = index;
+  img.setAttribute("focusIndex", index);
 
   img.addEventListener("click", function () {
     backgroundImage.src = images[index].src;
@@ -45,10 +47,35 @@ for (let index = 0; index < images.length; index++) {
   });
 
   img.addEventListener("keydown", function (event) {
-    if (event.key === " " || event.key === "Enter") {
+    if (event.key == " " || event.key == "Enter") {
       backgroundImage.src = images[index].src;
       backgroundImage.alt = images[index].alt;
       backgroundImage.title = images[index].title;
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    } else if (event.key == "ArrowRight") {
+      if (img.focusIndex + 1 < images.length) {
+        targetIndex = img.focusIndex + 1;
+      } else {
+        targetIndex = 0;
+      }
+
+      const newFocusTarget = document.querySelector(
+        `img[focusIndex="${targetIndex}"]`
+      );
+      newFocusTarget.focus();
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    } else if (event.key == "ArrowLeft") {
+      if (img.focusIndex - 1 >= 0) {
+        targetIndex = img.focusIndex - 1;
+      } else {
+        targetIndex = images.length - 1;
+      }
+
+      const newFocusTarget = document.querySelector(
+        `img[focusIndex="${targetIndex}"]`
+      );
+      newFocusTarget.focus();
     }
   });
 
